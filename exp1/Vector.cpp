2b939132b9018558e5da43cdea68a6715600ec17
lineea <3432430566@qq.com>
2024-10-06 20:23:26 +0800

    Update Vector.cpp

--- exp1/Vector.cpp
+++ exp1/Vector.cpp
@@ -58,3 +58,172 @@ public:
    void traverse ( void (* ) ( T& ) ); //遍历（使用函数指针，只读或局部性修改）
    template <typename VST> void traverse ( VST& ); //遍历（使用函数对象，可全局性修改）
 }; //Vector
+template <typename T>
+void Vector<T>::copyFrom(T const* A, Rank lo, Rank hi) {
+    _elem = new T[_capacity = 2 * (hi - lo)]; _size = 0;
+    while (lo < hi)
+        _elem[_size++] = A[lo++];
+} //复制的构造方法 
+template <typename T> Vector<T>& Vector<T>::operator= (Vector<T> const& V) { //重载 
+    if (_elem) delete[] _elem;
+    copyFrom(V._elem, 0, V.size());
+    return *this;
+}
+template <typename T> void Vector<T>::expand() {
+    if (_size < _capacity) return;
+    T* oldElem = _elem; copyFrom(oldElem, 0, _capacity);
+    delete[] oldElem;
+}//扩容 
+template <typename T> void Vector<T>::shrink() {
+    if (_capacity < DEFAULT_CAPACITY << 1) return;
+    if (_size << 2 > _capacity) return;
+    T* oldElem = _elem; _elem = new T[_capacity >>= 1];
+    for (Rank i = 0; i < _size; i++) _elem[i] = oldElem[i];
+    delete[] oldElem;
+}//缩容 
+template <typename T> T& Vector<T>::operator[] (Rank r) //重载下标操作符
+{
+    return _elem[r];
+}
+
+template <typename T> const T& Vector<T>::operator[] (Rank r) const //仅限于做右值
+{
+    return _elem[r];
+}//常规向量直接引用 
+template <typename T> void permute(Vector<T>& V) {
+    for (int i = V.size(); i > 0; i--)
+        swap(V[i - 1], V[rand() % i]);
+}//置乱算法 
+template <typename T> void Vector<T>::unsort(Rank lo, Rank hi) {
+    T* V = _elem + lo;
+    for (Rank i = hi - lo; 1 < i; --i)
+        swap(V[i - 1], V[rand() % i]);
+}//区间置乱接口 
+template <typename T> static bool lt(T* a, T* b) { return lt(*a, *b); }
+template <typename T> static bool lt(T& a, T& b) { return a < b; }
+template <typename T> static bool lt(T* a, T* b) { return lt(*a, *b); }
+template <typename T> static bool lt(T& a, T& b) { return a < b; }
+//重载比较器以比较对象指针 
+template <typename T>
+Rank Vector<T>::find(T const& e, Rank lo, Rank hi) const {
+    while ((lo < hi--) && (e != _elem[hi]));
+    return hi;
+}//顺序查找 
+template <typename T>
+Rank Vector<T>::insert(Rank r, T const& e) {
+    expand();
+    for (Rank i = _size; r < i; i--)
+        _elem[i] = _elem[i - 1];
+    _elem[r] = e; _size++;
+    return r;
+}//插入 
+template <typename T> Rank Vector<T>::remove(Rank lo, Rank hi) {
+    if (lo == hi) return 0;
+    while (hi < _size) _elem[lo++] = _elem[hi++];
+    _size = lo; shrink();
+    return hi - lo;
+}//区间删除 
+template <typename T> T Vector<T>::remove(Rank r) {
+    T e = _elem[r];
+    remove(r, r + 1);
+    return e;
+}//单元素删除 
+template <typename T> Rank Vector<T>::dedup() {
+    Rank oldSize = _size;
+    for (Rank i = 1; i < _size; )
+        if (-1 == find(_elem[i], 0, i))
+            i++;
+        else
+            remove(i);
+    return oldSize - _size;
+}//唯一化 
+template <typename T> void Vector<T>::traverse(void (*visit)(T&))
+{
+    for (Rank i = 0; i < _size; i++) visit(_elem[i]);
+} //遍历向量
+
+template <typename T> template <typename VST>
+void Vector<T>::traverse(VST& visit)
+{
+    for (Rank i = 0; i < _size; i++) visit(_elem[i]);
+} //遍历向量
+template <typename T> struct Increase
+{
+    virtual void operator()(T& e) { e++; }
+}; //假设T可直接递增或已重载++
+template <typename T> void increase(Vector<T>& V)
+{
+    V.traverse(Increase<T>());
+} //以Increase<T>()为基本操作进行遍历
+template <typename T> Rank Vector<T>::uniquify() {
+    Rank i = 0, j = 0;
+    while (++j < _size)
+        if (_elem[i] != _elem[j])
+            _elem[++i] = _elem[j];
+    _size = ++i; shrink();
+    return j - i;
+} //有序向量重复元素剔除算法（高效版）
+template <typename T>
+Rank Vector<T>::search(T const& e, Rank lo, Rank hi) const {
+    return (rand() % 2) ? binSearch(_elem, e, lo, hi) : fibSearch(_elem, e, lo, hi);
+} //等概率地随机使用二分查找、Fibonacci查找
+template <typename T> static Rank fibSearch(T* S, T const& e, Rank lo, Rank hi) {
+    for (Fib fib(hi - lo); lo < hi; ) {
+        while (hi - lo < fib.get()) fib.prev();
+        Rank mi = lo + fib.get() - 1;
+        if (e < S[mi]) hi = mi;
+        else if (S[mi] < e) lo = mi + 1;
+        else                  return mi;
+    }
+    return -1;
+}//fibonacci查找算法 
+template <typename T> static Rank binSearch(T* S, T const& e, Rank lo, Rank hi) {
+    while (lo < hi) {
+        Rank mi = (lo + hi) >> 1;
+        (e < S[mi]) ? hi = mi : lo = mi + 1;
+    }
+    return lo - 1;
+}//二分查找算法（版本C）：在有序向量的区间[lo, hi)内查找元素e，0 <= lo <= hi <= _size
+template <typename T> void Vector<T>::sort(Rank lo, Rank hi) {
+    switch (rand() % 5) {
+    case 1: bubbleSort(lo, hi); break;
+    case 2: selectionSort(lo, hi); break;
+    case 3: mergeSort(lo, hi); break;
+    case 4: heapSort(lo, hi); break;
+    case 5: quickSort(lo, hi); break;
+    default: shellSort(lo, hi); break;
+    }
+}//向量排序器接口
+template <typename T>
+void Vector<T>::bubbleSort(Rank lo, Rank hi)
+{
+    while (!bubble(lo, hi--));
+}//起泡排序
+template <typename T> bool Vector<T>::bubble(Rank lo, Rank hi) {
+    bool sorted = true;
+    while (++lo < hi)
+        if (_elem[lo - 1] > _elem[lo]) {
+            sorted = false;
+            swap(_elem[lo - 1], _elem[lo]);
+        }
+    return sorted;
+}//扫描交换单趟 
+template <typename T>
+void Vector<T>::mergeSort(Rank lo, Rank hi) {
+    if (hi - lo < 2) return;
+    Rank mi = (lo + hi) / 2;
+    mergeSort(lo, mi); mergeSort(mi, hi);
+    merge(lo, mi, hi);
+}//向量归并排序 
+template <typename T>
+void Vector<T>::merge(Rank lo, Rank mi, Rank hi) {
+    Rank i = 0; T* A = _elem + lo;
+    Rank j = 0, lb = mi - lo; T* B = new T[lb];
+    for (Rank i = 0; i < lb; i++) B[i] = A[i];
+    Rank k = 0, lc = hi - mi; T* C = _elem + mi;
+    while ((j < lb) && (k < lc))
+        A[i++] = (B[j] <= C[k]) ? B[j++] : C[k++];
+    while (j < lb)
+        A[i++] = B[j++];
+    delete[] B;
+}//有序向量二路归并 
